c4ce4e6cd710d1b61f3ad13c83de5c8fd7e879ef
NguyenChuong10 <Nguyenchuong22062002@gmail.com>
2023-05-10 22:01:45 +0700

    local variable

--- src/com/company/PrefixPostFix/PreFixPostFix.java
+++ src/com/company/PrefixPostFix/PreFixPostFix.java
@@ -7,9 +7,14 @@ public class PreFixPostFix {
         int firstVariable = 5;
         int secondVariable = 7;
         int result = firstVariable++ + ++secondVariable - 8;
-        System.out.println("firstVariable"+firstVariable);
-        System.out.println("secondVariable"+secondVariable);
-        System.out.println("result"+result);
+        System.out.println("firstVariable : " +firstVariable);
+        System.out.println("secondVariable : " +secondVariable);
+        System.out.println("result : " +result);
     }
+    /*  ++secondVariable là tiền tố : preefix
+        bước 1 :  sử lý tiền tố trước
+        bước 2 :  sử lý các phép toán còn lại sau đó trả về biến bên trái dấu bằng
+        bước 3 :  cuối cùng sử lý hậu tố của nó
+        firstVariable-- là hậu tố */
 
 }
---
+++ src/com/company/localvariable/LocalVariable.java
@@ -0,0 +1,10 @@
+package com.company.localvariable;
+
+public class LocalVariable {
+    public static void main(String[] args) {
+        int fisrtVariable = 5;// Đây là biến cục bộ
+        int secondVariable = 7;// Đây là biến cục bbô        System.out.println("firstVariable : " + fisrtVariable);
+        System.out.println("secondVariable : "+ secondVariable);
+
+    }
+}
