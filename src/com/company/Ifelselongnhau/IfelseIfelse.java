3d98cf009e0045103b5a33668117aa566b1a8914
NguyenChuong10 <Nguyenchuong22062002@gmail.com>
2023-05-18 17:11:24 +0700

    IfelseIfelse

---
+++ src/com/company/Ifelselongnhau/IfelseIfelse.java
@@ -0,0 +1,21 @@
+package com.company.Ifelselongnhau;
+
+import java.util.Scanner;
+
+public class IfelseIfelse {
+    public static void main(String[] args) {
+        float number;
+        Scanner scanner = new Scanner(System.in) ;
+        System.out.println(" nhap so can so sanh " );
+        number =scanner.nextFloat();
+
+        if(number > 10){
+            System.out.println("So nhap lon hon 10");
+        }else if(number == 10){
+            System.out.println("So nhap bang 10");
+
+        }else{
+            System.out.println("so nhap be hon 10 ");
+        }
+    }
+}
