89759faec6317777ff6d9865ca3648ea673c5ea5
NguyenChuong10 <Nguyenchuong22062002@gmail.com>
2023-12-24 15:27:47 +0700

    chuong

--- src/com/company/baitapfor/HienThi20SoNguyenDuongDauTienKhongCoBieuThuc1.java
+++ src/com/company/baitapfor/HienThi20SoNguyenDuongDauTienKhongCoBieuThuc1.java
@@ -1,9 +1,12 @@
 package com.company.baitapfor;
 
-public class HienThi20SoNguyenDuongDauTien {
+public class HienThi20SoNguyenDuongDauTienKhongCoBieuThuc1 {
+
     public static void main(String[] args) {
-        for(int i = 0 ; i <= 20 ; i++){
+        int i = 0 ;
+        for (  ; i<= 20 ; i++){
             System.out.println(i);
         }
     }
+
 }
