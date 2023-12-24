a4f60008623ae054a0ebcbf779f5fb949d38811d
NguyenChuong10 <Nguyenchuong22062002@gmail.com>
2023-12-24 15:36:32 +0700

    for3

---
+++ src/com/company/baitapfor/HienThi20SoNguyenDuongDauTienKhongcoBT1VaBT2.java
@@ -0,0 +1,13 @@
+package com.company.baitapfor;
+
+public class HienThi20SoNguyenDuongDauTienKhongcoBT1VaBT2 {
+
+    public static void main(String[] args ){
+        int i = 0;
+        for(;i <= 20 ;) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
